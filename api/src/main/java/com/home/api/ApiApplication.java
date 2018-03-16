213a5b674e742d09f2e4bcaa11afbe61ca6f4eee
knockinHeavenDoor <61239289@qq.com>
2018-03-16 20:57:12 +0800

    开启一些模块的配置

--- api/src/main/java/com/home/api/ApiApplication.java
+++ api/src/main/java/com/home/api/ApiApplication.java
@@ -2,8 +2,16 @@ package com.home.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker  //开启断路器支持
+@EnableZuulProxy
 public class ApiApplication {
 
 	public static void main(String[] args) {
