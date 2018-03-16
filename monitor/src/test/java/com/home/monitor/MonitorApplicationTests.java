e350347552e8459ad3e4ed28681761516bce84f7
knockinHeavenDoor <61239289@qq.com>
2018-03-16 23:38:10 +0800

    监控器模块

--- monitor/src/test/java/com/home/monitor/MonitorApplicationTests.java
+++ monitor/src/test/java/com/home/monitor/MonitorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.home.api;
+package com.home.monitor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApiApplicationTests {
+public class MonitorApplicationTests {
 
 	@Test
 	public void contextLoads() {
