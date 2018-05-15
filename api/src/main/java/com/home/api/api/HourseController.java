ba022e296c154e166528e70663f2f1a941ed8069
knockinHeavenDoor <61239289@qq.com>
2018-05-16 00:05:16 +0800

    定义hourse的feign接口

--- api/src/main/java/com/home/api/api/HourseController.java
+++ api/src/main/java/com/home/api/api/HourseController.java
@@ -1,7 +1,12 @@
 package com.home.api.api;
 
+import com.home.api.schedual.SchedualHourse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HourseController {
+
+    @Autowired
+    SchedualHourse schedualHourse;
 }
--- api/src/main/java/com/home/api/schedual/SchedualHourse.java
+++ api/src/main/java/com/home/api/schedual/SchedualHourse.java
@@ -1,9 +1,26 @@
 package com.home.api.schedual;
 
+import model.BaseHourse;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import util.ApiResponse;
+import util.NoPagingResponse;
+import util.PageRequest;
 
 @FeignClient(value = "hourse")
 @Service
 public interface SchedualHourse {
+
+    @RequestMapping(value = "/api/hourse/{hourseId}",method = RequestMethod.GET)
+    NoPagingResponse login(@PathVariable("hourseId") String hourseId);
+
+    @RequestMapping(value = "/api/update",method = RequestMethod.PUT)
+    NoPagingResponse update(@RequestBody BaseHourse hourse, @RequestParam("type")String type);
+
+    @RequestMapping(value = "/api/delete/{hourseId}",method = RequestMethod.DELETE)
+    NoPagingResponse delete(@PathVariable("hourseId") String hourseId,@RequestParam("userId")String userId);
+
+    @RequestMapping("/api/front/hourses/{type}")
+    ApiResponse getAllHourse(@PathVariable("type") String type, PageRequest pageRequest);
 }
